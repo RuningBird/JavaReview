f8cb992c3d9e667512a9ed256f83ff47b0f76314
hr <ipowerink@outlook.com>
2017-08-22 14:36:46 +0800

    增强for循环

--- src/Review01.java
+++ src/Review01.java
@@ -1,8 +1,36 @@
 /**
  * Created by hr on 2017/8/22.
  */
+
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Review01 {
-    public static void main(String args[]){
-        
+
+    public static void main(String args[]) {
+//        showArray();
+        showForEach();
+    }
+
+    /**
+     * 增强for循环
+     */
+    public static void showForEach(){
+        int[] arr = {1,3,4,5,6};
+        for (int x: arr) {
+            System.out.println(x);
+        }
+
+    }
+
+    public static void showArray() {
+        Integer[] arr = {1, 2, 3, 4, 5};
+        Integer[] arr2 = {1, 2, 3, 4, 5};
+
+        int[] arr3 = {4,3,4,5};
+
+        System.out.println(arr);
+        boolean b = Arrays.deepEquals(arr, arr2);
+        System.out.println(b);
     }
 }
